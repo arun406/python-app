44542263e6fadf29de4f7d3dffd9f00de6b5a137
Arun Kumar Kandakatla <k.arun406@gmail.com>
2025-10-03 21:57:02 +0530

    download path changed.

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3. I am awesome !!!',
+        'message': 'You are doing great, little human! <3. I am awesome !!@!',
         'deployed_on': 'kubernetes'
     })
 
